56936d8eefe86ecc8aadc36c5d5fafe1c1364686
OrvilSavery <orvilsavery@gmail.com>
2015-10-01 17:56:12 -0500

    Update header.blade.php

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -5,7 +5,6 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
-    <meta name="viewport" content="initial-scale=1.0">
     <link rel="icon" href="../../favicon.ico">
 
     <title>Spontanunity</title>
