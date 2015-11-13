2f75669bce9d537bdc3e74bd7d84468b8bc6f50f
Tarun Krishnan <tarun@tarunkrishnan.com>
2015-11-13 08:10:42 -0500

    updated viewport

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -2,8 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Spontanuity</title>
     <link rel="stylesheet" href="{{ URL::asset('library/css/bootstrap.css') }}">
     <script src="{{ URL::asset('library/bower_components/jquery/dist/jquery.js') }}"></script>
     <script src="{{ URL::asset('library/js/bootstrap.js') }}"></script>
+
 </head>
