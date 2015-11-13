154c35f0bf012052e27f3c93d1f38c9250186da5
Tarun Krishnan <tarun@tarunkrishnan.com>
2015-11-13 08:28:43 -0500

    updated typography

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -7,5 +7,6 @@
     <link rel="stylesheet" href="{{ URL::asset('library/css/bootstrap.css') }}">
     <script src="{{ URL::asset('library/bower_components/jquery/dist/jquery.js') }}"></script>
     <script src="{{ URL::asset('library/js/bootstrap.js') }}"></script>
-
+    <script src="https://use.typekit.net/qvm1uhq.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
 </head>
