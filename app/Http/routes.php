20198646395699afe29e27fecf71c962c5e4f8fa
Tarun Krishnan <tarun@tarunkrishnan.com>
2016-02-01 00:03:28 -0500

    updated routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,7 +21,6 @@ Route::get('register', 'Auth\AuthController@getRegister');
 Route::post('register', 'Auth\AuthController@postRegister');
 
 get('/', 'PagesController@index');
-get('home', function(){ return redirect('/')});
 Route::group(['prefix' => 'user'], function () {
     get('name-and-gender', 'PagesController@userSetNameAndGender');
     post('name-and-gender', 'UserController@updateNameAndGender');
